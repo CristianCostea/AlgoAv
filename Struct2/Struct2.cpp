b906d639add05497a8bf64e71eb6fa7ba6919641
Cristian Costea <cristian.costea@treekos.ro>
2023-02-11 14:46:54 +0200

    11.02

--- Struct2/Struct2.cpp
+++ Struct2/Struct2.cpp
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 typedef struct laptop
-{
+{ 
 	char tipcpu[6];
 	float frecventa; //in GHZ
 	float memorie; //in GB
@@ -29,14 +29,25 @@ int  bubbleSort(laptop a[10], int lung) {
 	return 0;
 }
 
+int  ixNumBubbleSort(float ix[][2], int lung) {
+	for (int i = 0; i < lung; i++) {
+		for (int j = 0; j < i; j++) {
+			if (ix[j][1] > ix[i][1]) {
+				swap(ix[i], ix[j]);
+			}
+		}
+	}
+	return 0;
+}
+
 int BinarySearch(laptop a[],int n, float frecventa)
 // returneaza -1 daca nu gaseste rez sau ID-ul elementului gasit
 {
 	int i = 0;
 	int j = n - 1;
 	int k = (i+j) / 2;
-	cout << "i j k\n";
-	cout << i << " " << j << " " << k << '\n';
+	std::cout << "i j k\n";
+	std::cout << i << " " << j << " " << k << '\n';
 
 	while (i<=j)
 	{
@@ -60,9 +71,88 @@ int BinarySearch(laptop a[],int n, float frecventa)
 	}
 	return -1;
 }
+
+int ixNumBinarySearch(float ix[][2], int n, float num)
+// returneaza -1 daca nu gaseste rez sau ID-ul elementului gasit
+{
+	int i = 0;
+	int j = n - 1;
+	int k = (i + j) / 2;
+	cout << "i j k\n";
+	cout << i << " " << j << " " << k << '\n';
+
+	while (i <= j)
+	{
+		if (ix[k][1] == num)
+		{
+			return k;
+		}
+		if (ix[k][1] < num)
+		{
+			i = k + 1;
+			k = (i + j) / 2;
+			cout << i << " " << j << " " << k << '\n';
+		}
+		if (ix[k][1] > num)
+		{
+			j = k - 1;
+			k = (i + j) / 2;
+			cout << i << " " << j << " " << k << '\n';
+		}
+
+	}
+	return -1;
+}
+
+int ixNumInsert(float ix[][2], int n, int id, float v)
+// returneaza -1 daca nu gaseste rez sau ID-ul elementului gasit
+{
+	int i = 0;
+	int j = n - 1;
+	int k = (i + j) / 2;
+	//cout << "i j k\n";
+	//cout << i << " " << j << " " << k << '\n';
+
+	while (i <= j)
+	{
+		if (ix[k][1] == v)
+		{
+			for (int l = n ; l >= k + 1; l--) {
+				ix[l][0] = ix[l - 1][0];
+				ix[l][1] = ix[l - 1][1];
+			}
+			ix[k][0] = id;
+			ix[k][1] = v;
+			return n + 1;
+		}
+		if (ix[k][1] < v)
+		{
+			i = k + 1;
+			k = (i + j) / 2;
+//			cout << i << " " << j << " " << k << '\n';
+		}
+		if (ix[k][1] > v)
+		{
+			j = k - 1;
+			k = (i + j) / 2;
+//			cout << i << " " << j << " " << k << '\n';
+		}
+
+	}
+	for (int l = n ; l >= j + 1; l--) {
+		ix[l][0] = ix[l - 1][0];
+		ix[l][1] = ix[l - 1][1];
+	}
+	ix[j+1][0] = id;
+	ix[j+1][1] = v;
+	return n + 1;
+
+}
+
 int main()
 {
-	laptop al[10];
+	laptop al[100];
+	float ixFrecv[100][2];
 	int n;
 	ifstream fin("in.txt");
 	ofstream fout("out.txt");
@@ -80,19 +170,22 @@ int main()
 		al[i].id = i;
 	}
 
-	bubbleSort(al, n);
-
-
+	for (int i = 0; i < n; i++) {
+		ixFrecv[i][0] = al[i].id;
+		ixFrecv[i][1] = al[i].frecventa;
+	}
+	ixNumBubbleSort(ixFrecv, n);
+	n = ixNumInsert(ixFrecv, n, n, 1.7);
 	std::cout << "Ai introdus, sortat dupa frecventa crescator\n";
 	for (int i=0; i < n; i++) {
-		std::cout <<al[i].id << " " << al[i].tipcpu << " " << al[i].frecventa << " "
-			<< al[i].memorie << " " << al[i].tipdisk << " " << al[i].capacitatedisk << "\n";
+		std::cout <<al[int(ixFrecv[i][0])].id << " " << al[int(ixFrecv[i][0])].tipcpu << " " << al[int(ixFrecv[i][0])].frecventa << " "
+			<< al[int(ixFrecv[i][0])].memorie << " " << al[int(ixFrecv[i][0])].tipdisk << " " << al[int(ixFrecv[i][0])].capacitatedisk << "\n";
 	}
 	cout << n << '\n';
 	std::cout << "Ce frecventa cautati?";
 	float f;
 	std::cin >> f;
-	int pozf = BinarySearch(al, n, f);
+	int pozf = ixNumBinarySearch(ixFrecv, n, f);
 	if (pozf > -1)
 	{
 		fout << "Am gasit frecventa " << f << " in pozitia " << pozf << '\n';
